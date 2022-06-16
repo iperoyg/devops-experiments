a67fb2e3bf5d6f7f3ea311e4f4da3ca0ee4b5f6c
Arthur Iperoyg <iperoyg@gmail.com>
2022-06-16 11:47:02 -0300

    add first complexoperation - just some math

---
+++ src/complexoperations/JustSomeMath.py
@@ -0,0 +1,17 @@
+from mimetypes import init
+
+
+import math
+import random
+
+class JustSomeMath:
+    def __init__(self) -> None:
+        pass
+
+    def operate(self, number:int = 0) -> int:
+        '''
+        Does an exponentiation operation on the passed number using a random exponent
+        '''
+        random_int = random.randint(0, number)
+        return math.pow(number, random_int)
+
