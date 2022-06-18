87c157370de56f48d6b90e53314b3ff6295c1995
Arthur Iperoyg <iperoyg@gmail.com>
2022-06-18 12:48:32 -0300

    fix lint errors in complex operations files

--- src/complexoperations/JustSomeMath.py
+++ src/complexoperations/JustSomeMath.py
@@ -1,17 +1,16 @@
-from mimetypes import init
-
-
 import math
 import random
 
+
 class JustSomeMath:
+
     def __init__(self) -> None:
         pass
 
-    def operate(self, number:int = 0) -> int:
+    def operate(self, number: int = 0) -> int:
         '''
-        Does an exponentiation operation on the passed number using a random exponent
+        Does an exponentiation operation on the passed number
+        using a random exponent
         '''
         random_int = random.randint(0, number)
         return math.pow(number, random_int)
-
--- src/complexoperations/JustSomeString.py
+++ src/complexoperations/JustSomeString.py
@@ -1,14 +1,13 @@
-
-
 class JustSomeString:
     def __init__(self) -> None:
         pass
 
-    def operate(self, text:str, number:int=1) -> int:
+    def operate(self, text: str, n: int = 1) -> int:
         '''
-        Replicates the text for the number of times informed in the 'number' 
-        parameter and then removes the elements in multiple positions of the same parameter
+        Replicates the text for the n of times informed in the 'n'
+        parameter and then removes the elements in multiple positions of the
+        same parameter
         '''
-        replicated = text * number
-        cleaned = ''.join([e for i, e in enumerate(replicated) if i % number != 0])
+        replicated = text * n
+        cleaned = ''.join([e for i, e in enumerate(replicated) if i % n != 0])
         return cleaned
