5c29e3b02f0457a495fa42ef7171c007dd365a77
Arthur Iperoyg <iperoyg@gmail.com>
2022-06-18 17:29:40 -0300

    add documentation for string operations class

--- src/complexoperations/JustSomeString.py
+++ src/complexoperations/JustSomeString.py
@@ -1,5 +1,10 @@
 class JustSomeString:
+    """
+    Provides `nonsense` string operations for educational purposes only
+    """
+
     def __init__(self) -> None:
+        """String  operations init"""
         pass
 
     def operate(self, text: str, n: int = 1) -> int:
@@ -7,6 +12,11 @@ class JustSomeString:
         Replicates the text for the n of times informed in the 'n'
         parameter and then removes the elements in multiple positions of the
         same parameter
+
+        :param str text: The text to be replicated and cropped
+        :param int n: The number of replications and the position
+            multiple for removal
+        :return string: the operation result
         '''
         replicated = text * n
         cleaned = ''.join([e for i, e in enumerate(replicated) if i % n != 0])
