79bb39460825cb3e19b20942b1a1bd29139c6e3c
Arthur Iperoyg <iperoyg@gmail.com>
2022-06-26 20:23:36 -0300

    improve linting and lint check for api

--- src/apicomplexoperations/up.py
+++ src/apicomplexoperations/up.py
@@ -2,12 +2,16 @@ import sys
 import uvicorn
 import subprocess
 
-subprocess.check_call([sys.executable, "-m", "pip", "install", "--no-index", "--find-links=dep/", "complexoperations-iperoyg"])
+pkg = "complexoperations-iperoyg"
+noindex = "--no-index"
+folder = "--find-links=dep/"
+params = [sys.executable, "-m", "pip", "install", noindex, folder, pkg]
+subprocess.check_call(params)
 
 from main import app
 
 apiapp = app
-port = int(sys.argv[1])    
+port = int(sys.argv[1])
 
 if __name__ == "__main__":
     uvicorn.run("up:apiapp", host="127.0.0.1", port=port, log_level="trace")
