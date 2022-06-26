fbf915682a7031ddc4a537c4efeac0ab48d706bc
Arthur Iperoyg <iperoyg@gmail.com>
2022-06-26 11:44:05 -0300

    add endpoints for complexoperations

--- src/apicomplexoperations/main.py
+++ src/apicomplexoperations/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from complexoperations import JustSomeMath, JustSomeString
 
 app = FastAPI()
+mathop = JustSomeMath.JustSomeMath()
+stringop = JustSomeString.JustSomeString()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/math/{number}")
+async def complex_math(number: int = 0):
+    return {"message": mathop.operate(number)}
+
+
+@app.get("/string")
+async def complex_string(text: str, number: int = 1):
+    return {"message": stringop.operate(text, number)}
