0bc8b75979665f3fbe4b29fa6651bd796014a112
Arthur Iperoyg <iperoyg@gmail.com>
2022-06-26 15:56:02 -0300

    add health check to api

--- src/apicomplexoperations/main.py
+++ src/apicomplexoperations/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from complexoperations import JustSomeMath, JustSomeString
 
 app = FastAPI()
@@ -19,3 +19,12 @@ async def complex_math(number: int = 0):
 @app.get("/string")
 async def complex_string(text: str, number: int = 0):
     return {"message": stringop.operate(text, number)}
+
+
+@app.get("/health")
+async def health_check():
+    if mathop is not None and stringop is not None:
+        return {"message": "Healthy"}
+    else:
+        msg = "We have problem! Complex Operation are None"
+        HTTPException(status_code=500, detail=msg)
