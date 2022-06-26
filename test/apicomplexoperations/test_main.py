79816501f93bdd3d52dc3b13b4b280dc3c8695c7
Arthur Iperoyg <iperoyg@gmail.com>
2022-06-26 12:33:36 -0300

    Add CI workflow and tests for API

---
+++ test/apicomplexoperations/test_main.py
@@ -0,0 +1,23 @@
+from apicomplexoperations.main import app
+from fastapi.testclient import TestClient
+
+
+client = TestClient(app)
+
+
+class TestFastAPIMain:
+
+    def test_root_get(self):
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"message": "Hello World"}
+
+    def test_math_get(self):
+        response = client.get("/math/0")
+        assert response.status_code == 200
+        assert response.json() == {"message": 1}
+
+    def test_string_get(self):
+        response = client.get("/string?text=abc&number=2")
+        assert response.status_code == 200
+        assert response.json() == {"message": "bac"}
